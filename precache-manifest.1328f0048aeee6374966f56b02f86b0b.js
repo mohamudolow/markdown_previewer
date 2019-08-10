e4a912199d12922bbee295ec24a9f690314acb4f
Mohamud Olow <khalif18@live.com>
2019-08-10 15:50:26 -0700

    Updates

---
+++ precache-manifest.1328f0048aeee6374966f56b02f86b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c282506ce660807e94328d7877dc5e36",
+    "url": "/markdown_previewer/index.html"
+  },
+  {
+    "revision": "a98052389aa300988d42",
+    "url": "/markdown_previewer/static/css/main.804c4bdb.chunk.css"
+  },
+  {
+    "revision": "f8b9affef82867f7f884",
+    "url": "/markdown_previewer/static/js/2.c615a857.chunk.js"
+  },
+  {
+    "revision": "a98052389aa300988d42",
+    "url": "/markdown_previewer/static/js/main.8693510b.chunk.js"
+  },
+  {
+    "revision": "b1bbfec7a91568f74675",
+    "url": "/markdown_previewer/static/js/runtime~main.db5017de.js"
+  }
+]);
